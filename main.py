e4d6de296e7867377c4cdbe1a691f17540546abc
andreprisya <andre.p20ti@mahasiswa.pcr.ac.id>
2023-01-25 17:36:13 +0700

    fix output

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if st.button('Make Prediction'):
         [int(input_prediksi_horsepower)], 0)
     prediction = best_xgboost_model.predict(inputs)
     print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Harga Mobil Adalah : {np.squeeze(prediction, -1):.2f}g")
+    st.write(f"Harga Mobil Adalah : {np.squeeze(prediction, -1)} $")
 
     # st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
     # st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
